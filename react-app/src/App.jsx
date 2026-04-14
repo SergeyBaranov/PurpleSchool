dc4819b97d64a00a8a9851a4d70054d44cc1e0f8
Sergey <workinggrid@gmail.com>
2026-04-14 18:52:39 +0300

    start

---
+++ react-app/src/App.jsx
@@ -0,0 +1,41 @@
+
+import './App.css';
+import Button from './components/Button/Button';
+import CardButton from './components/CardButton/CardButton';
+import JournalItem from './components/JournalItem/JournalItem';
+
+function App() {
+
+  const data = [
+    {
+      title: 'Подготовка к обновлению курсов',
+      text: 'Горные походы открывают удивительные природные ландшафты',
+      date: new Date()
+    },
+    {
+      title: 'Поход в годы',
+      text: 'Большое значение в горном туризме придается бытовому обустройству',
+      date: new Date()
+    }
+  ];
+
+  return (
+    <>
+      <h1>Заголовок</h1>
+      <p>Какой-то текст</p>
+      <Button />
+      <CardButton  >
+        Добавить пункт
+      </CardButton>
+      <CardButton  >
+        <JournalItem 
+          title={data[1].title}
+          text={data[1].text}
+          sate={data[1].date}
+        />
+      </CardButton>
+    </>
+  );
+}
+
+export default App;
