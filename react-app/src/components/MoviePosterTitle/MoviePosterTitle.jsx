c7b6649e817ece99e6861c2271528c7e20e34406
Sergey <workinggrid@gmail.com>
2026-06-17 05:55:22 +0300

    moved to TS

--- react-app/src/components/MoviePosterTitle/MoviePosterTitle.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from './MoviePosterTitle.module.css';
-
-function MoviePosterTitle({ title }) {
-    return <h3 className={styles['movie-title']}>{title}</h3>;  // Или другой элемент
-}
-
-export default MoviePosterTitle;
